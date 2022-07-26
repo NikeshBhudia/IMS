d2e7ce3d250c1d0c48d5de890ddee2465f19c202
nikeshbhudia <nikeshbhudia1@gmail.com>
2022-07-26 18:13:05 +0100

    Configured Schema file to create the database

--- src/main/resources/sql-schema.sql
+++ src/main/resources/sql-schema.sql
@@ -1,12 +1,38 @@
-drop schema ims;
+drop schema if exists ims;
 
-CREATE SCHEMA IF NOT EXISTS `ims`;
+CREATE SCHEMA IF NOT EXISTS ims;
 
-USE `ims` ;
+USE ims ;
 
-CREATE TABLE IF NOT EXISTS `ims`.`customers` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT,
-    `first_name` VARCHAR(40) DEFAULT NULL,
-    `surname` VARCHAR(40) DEFAULT NULL,
-    PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS customers (
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(40) NOT NULL,
+    surname VARCHAR(40) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE IF NOT EXISTS orders (
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    DueDate DATE NOT NULL,
+    Cost DECIMAL NOT NULL,
+    Customer_id INT(10) NOT NULL,
+    No_Of_Items INT(10) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (Customer_id) REFERENCES customers(id)
+);
+
+CREATE TABLE IF NOT EXISTS item (
+    id INT(10) NOT NULL AUTO_INCREMENT,
+    Item_name VARCHAR(40) NOT NULL,
+    Price DECIMAL NOT NULL,
+    PRIMARY KEY (id)  
+);
+
+CREATE TABLE IF NOT EXISTS itemrecipt (
+    id INT NOT NULL AUTO_INCREMENT,
+    ItemID INT(10) NOT NULL,
+    OrderID INT(10) NOT NULL,
+    PRIMARY KEY (id),
+    FOREIGN KEY (ItemID) REFERENCES item(id),
+    FOREIGN KEY (OrderID) REFERENCES orders(id)
 );
