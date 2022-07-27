fb4b1ac84de99139100d8bc7b5a0680c74464e83
nikeshbhudia <nikeshbhudia1@gmail.com>
2022-07-27 13:42:43 +0100

    removed extra import file within ItemDAO

--- src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
+++ src/main/java/com/qa/ims/persistence/dao/ItemDAO.java
@@ -10,7 +10,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import com.qa.ims.persistence.domain.Customer;
+
 import com.qa.ims.persistence.domain.Item;
 import com.qa.ims.utils.DBUtils;
 
